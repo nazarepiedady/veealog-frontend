f3f94a1e053cefaa6249d10bda07b69245150c91
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-07 17:33:08 +0100

    feat: import `post` component and register it as a view

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+//import HomeView from '../views/HomeView.vue'
+import Post from '@/components/Post.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  { path: '/post/:slug', component: Post }
 ]
 
 const router = new VueRouter({
