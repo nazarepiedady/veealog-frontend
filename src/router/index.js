ffc3bab57f1d941bbc1944ba48dfdefd986096b8
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-07 17:34:45 +0100

    feat: import `author` component and register it as a view

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 //import HomeView from '../views/HomeView.vue'
-import Post from '@/components/Post.vue'
+import Post from '@/components/Post'
+import Author from '@/components/Author'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/author/:username', component: Author },
   { path: '/post/:slug', component: Post }
 ]
 
