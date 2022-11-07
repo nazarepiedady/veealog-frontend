ad8b94f38ab6bfc394bc7ff17343e04eebf8a9ff
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-07 18:33:18 +0100

    chore: create the `author` component

---
+++ src/components/Author.vue
@@ -0,0 +1,34 @@
+<template>
+	<div v-if="author">
+		<h2>{{ displayName }}</h2>
+		<a :href="author.website" rel="noopener noreferrer">Website</a>
+		<p>{{ author.bio }}</p>
+		<h3>Posts by {{ displayName }}</h3>
+		<PostList :showAuthor="false" :posts="author.postSet" />
+	</div>
+</template>
+
+<script>
+	import PostList from '@/components/PostList'
+
+	export default {
+		name: 'Author',
+		components: {
+			PostList,
+		},
+		data() {
+			return {
+				author: null,
+			}
+		},
+		computed: {
+			displayName() {
+				return (
+					this.author.user.firstName &&
+					this.author.user.lastName &&
+					`${this.author.user.firstName} ${this.author.user.lastName}`
+				) || `${this.author.user.username}`
+			}
+		}
+	}
+</script>
