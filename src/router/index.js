f9fd8e007e9c0dd7b79db091f5ff4c96efc6ec5b
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-07 17:37:38 +0100

    feat: import `postsbytag` component and register it as a view

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,14 @@ import VueRouter from 'vue-router'
 //import HomeView from '../views/HomeView.vue'
 import Post from '@/components/Post'
 import Author from '@/components/Author'
+import PostsByTag from '@/components/PostsByTag'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/author/:username', component: Author },
-  { path: '/post/:slug', component: Post }
+  { path: '/post/:slug', component: Post },
+  { path: '/tag/:tag', component: PostsByTag },
 ]
 
 const router = new VueRouter({
