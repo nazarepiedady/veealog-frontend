03d40fd2cbf8cba7f21315dc872664617498b5db
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-07 18:25:40 +0100

    chore: create the `posts-by-tag` component

---
+++ src/components/PostsByTag.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h2>Posts in #{{ $route.params.tag }}</h2>
+		<PostList v-if="posts" :posts="posts" />
+	</div>
+</template>
+
+<script>
+	import PostList from '@/components/PostList'
+
+	export default {
+		name: 'PostsByTag',
+		components: {
+			PostList,
+		},
+		data() {
+			return {
+				posts: null,
+			}
+		},
+	}
+</script>
