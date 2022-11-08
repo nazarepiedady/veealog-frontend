292f262296b958584eb292c86b000681204949b8
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-08 19:35:09 +0100

    chore: uncomment the `all-posts` view definition

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router'
 //import Post from '@/components/Post'
 import Author from '@/components/Author'
 import PostsByTag from '@/components/PostsByTag'
-//import AllPosts from '@/components/AllPosts'
+import AllPosts from '@/components/AllPosts'
 
 Vue.use(VueRouter)
 
@@ -12,7 +12,7 @@ const routes = [
   { path: '/author/:username', component: Author },
   //{ path: '/post/:slug', component: Post },
   { path: '/tag/:tag', component: PostsByTag },
-  //{ path: '/', component: AllPosts },
+  { path: '/', component: AllPosts },
 ]
 
 const router = new VueRouter({
