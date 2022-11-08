05681b80c7c1250d415fbfa9b38761128b8ba6c0
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-08 04:00:37 +0100

    chore: comment the routes that are not being used yet

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Post from '@/components/Post'
+//import Post from '@/components/Post'
 import Author from '@/components/Author'
 import PostsByTag from '@/components/PostsByTag'
-import AllPosts from '@/components/AllPosts'
+//import AllPosts from '@/components/AllPosts'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/author/:username', component: Author },
-  { path: '/post/:slug', component: Post },
+  //{ path: '/post/:slug', component: Post },
   { path: '/tag/:tag', component: PostsByTag },
-  { path: '/', component: AllPosts },
+  //{ path: '/', component: AllPosts },
 ]
 
 const router = new VueRouter({
