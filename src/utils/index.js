f328e43f3e03b5395844bbe08d073046d4b12006
Nazaré da Piedade <nazarepiedady@gmail.com>
2022-11-10 22:27:25 +0100

    feat: create the `format date` utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,10 @@ export function formatUserName(firstName, lastName, userName) {
 	if (firstName && lastName) return `${firstName} ${lastName}`
 	return `${userName}`
 }
+
+export function formatDateToBeReadable(date) {
+	let dateLanguage = 'en-US'
+	let realDate = new Date(date)
+	let dateStyle = { dateStyle: 'full' }
+	return new Intl.DateTimeFormat(dateLanguage, dateStyle).format(realDate)
+}
